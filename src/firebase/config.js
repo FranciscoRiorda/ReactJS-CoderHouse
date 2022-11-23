cbde0a3ec626a01d7777ded266b7610d99757afe
Francisco Riorda <riorda7@gmail.com>
2022-11-23 12:55:58 -0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqZJsRmLc3ytrfDsd5IGi3HYy1lNmMRHU",
+  authDomain: "reactjs-coderhouse-4fe15.firebaseapp.com",
+  projectId: "reactjs-coderhouse-4fe15",
+  storageBucket: "reactjs-coderhouse-4fe15.appspot.com",
+  messagingSenderId: "1015516901081",
+  appId: "1:1015516901081:web:05b7c0aa53444c55d5d8c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
